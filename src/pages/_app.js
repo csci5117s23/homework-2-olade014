70fb09511b95b143eee0e583e228eec82ceda105
students <students@ESCI-lab-mba03.local>
2023-05-02 14:46:20 -0500

    Resolve Issues with codehooks

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,21 +6,21 @@ export default function App({ Component, pageProps }) {
   
   return (
     <ClerkProvider {...pageProps} 
-        appearance={{
-          baseTheme:light,
-          variables: {
-            colorPrimary:'#262626',
-            colorTextOnPrimaryBackground:'#ffffff',
-            colorTextSecondary: '#000000',
-            colorInputText: '#262626',
-            colorInputBackground: '#ffffff',
-            fontFamily: 'Poppins'
-          },
-          layout: {
-            socialButtonsPlacement:'bottom'
-          }
-        }}>
-        
+        // appearance={{
+        //   baseTheme:light,
+        //   variables: {
+        //     colorPrimary:'#262626',
+        //     colorTextOnPrimaryBackground:'#ffffff',
+        //     colorTextSecondary: '#000000',
+        //     colorInputText: '#262626',
+        //     colorInputBackground: '#ffffff',
+        //     fontFamily: 'Poppins'
+        //   },
+        //   layout: {
+        //     socialButtonsPlacement:'bottom'
+        //   }
+        // }}
+        >
         <Component {...pageProps} />
 
       </ClerkProvider>
